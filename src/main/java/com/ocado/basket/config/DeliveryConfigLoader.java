921b3c1409fffcae3a124c68de4440efa5151882
sit3kk <sito.270@interia.pl>
2024-04-07 22:37:26 +0200

    Config

--- src/main/java/com/ocado/basket/config/DeliveryConfigLoader.java
+++ src/main/java/com/ocado/basket/config/DeliveryConfigLoader.java
@@ -1,4 +1,19 @@
-package main.java.com.ocado.basket.config;
+package com.ocado.basket.config;
+
+import com.ocado.basket.utils.JsonUtils;
+
+import java.util.List;
+import java.util.Map;
 
 public class DeliveryConfigLoader {
+
+    private Map<String, List<String>> deliveryOptions;
+
+    public DeliveryConfigLoader(String absolutePathToConfigFile) {
+        this.deliveryOptions = JsonUtils.readJsonFromFile(absolutePathToConfigFile, Map.class);
+    }
+
+    public Map<String, List<String>> getDeliveryOptions() {
+        return deliveryOptions;
+    }
 }
